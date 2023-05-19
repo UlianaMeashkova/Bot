13379e05988131687eb4608c236ef3e0b9297f34
UlianaMeashkova <el.mahno@gmail.com>
2023-05-19 14:55:09 +0300

    swagger

--- bot.py
+++ bot.py
@@ -1,19 +1,18 @@
+import aioredis
 import os
 import logging
 
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
-import aioredis
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, ContextTypes, filters
 
+from utils import get_chat_ids
 
-
-with open("token.txt", "r") as f:
-    BOT_TOKEN = f.read()
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-redis = aioredis.from_url("redis://redis")
+redis = aioredis.from_url("redis://localhost")
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -21,11 +20,19 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         f"Start chat #{update.effective_chat.id} - username {update.effective_user.first_name}"
     )
     await redis.lpush("chats", update.effective_chat.id)
-
     await update.message.reply_text(
         f"Hello {update.effective_user.first_name}"
     )
 
+async def delete(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    logger.info(
+        f"Delete chat #{update.effective_chat.id} - username {update.effective_user.first_name}"
+    )
+    if update.effective_chat.id not in get_chat_ids():
+        await redis.lpush("chats", update.effective_chat.id)
+    await update.message.reply_text(
+        f"Hello {update.effective_user.first_name}"
+    )
 
 async def message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     logger.info(
--- message.py
+++ message.py
@@ -1,25 +1,26 @@
 import asyncio
 import os
+import asyncio
 import aioredis
-import random
+import os
 
 from telegram import Bot
 
-with open("token.txt", "r") as f:
-    BOT_TOKEN = f.read()
-
-
-redis = aioredis.from_url("redis://redis")
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+redis = aioredis.from_url("redis://localhost")
 
 
 async def send_message(chat_id: int, text: str) -> None:
     bot = Bot(BOT_TOKEN)
     await bot.send_message(chat_id=chat_id, text=text)
 
+
 async def send_message_to_all(text: str) -> None:
-     for index in range(0,await redis.llen("chats")):
-          chat_id = await redis.lindex("chats", index)
-          await send_message(chat_id=int(chat_id), text=text)
+    for chat_id in {
+        await redis.lindex("chats", index)
+        for index in range(0, await redis.llen("chats"))
+    }:
+        await send_message(chat_id=int(chat_id), text=text)
 
 
 if __name__ == "__main__":
@@ -27,5 +28,5 @@ if __name__ == "__main__":
     # text = input("Your message: ")
     # asyncio.run(send_message(chat_id=chat_id, text=text))
 
-     text = input("Your message: ")
-     asyncio.run(send_message_to_all(text=text))
+    text = input("Your message: ")
+    asyncio.run(send_message_to_all(text=text))
